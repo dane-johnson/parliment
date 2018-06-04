5f892b71ce4cbc1a9c663828d89e82f29ec2739c
Dane Johnson <daneallenjohnson@protonmail.com>
2018-06-04 15:31:53 -0500

    added http kit

--- src/clojure/parliment/core.clj
+++ src/clojure/parliment/core.clj
@@ -1,5 +1,20 @@
 (ns parliment.core
+  (:require [org.httpkit.server :refer :all])
   (:gen-class))
+
+(defonce server (atom nil))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World!"})
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(reset! server (run-server app {:port 8080}))
 
 (defn -main
   "I don't do a whole lot ... yet."
