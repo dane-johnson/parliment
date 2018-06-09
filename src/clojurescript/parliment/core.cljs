8c5974d57a20e299eb372bb9493851be90c4990e
Dane Johnson <daneallenjohnson@protonmail.com>
2018-06-08 21:44:05 -0500

    Added stuff

--- src/clojurescript/parliment/core.cljs
+++ src/clojurescript/parliment/core.cljs
@@ -1,16 +1,27 @@
 (ns parliment.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs.reader :refer [read-string]]))
 
 (enable-console-print!)
 
 (defonce socket (js/WebSocket. "ws://localhost:8080/ws"))
+(defonce gamestate (atom {}))
+
+(declare landing lobby)
+
+(defn render-page
+  [page]
+  (reagent/render-component [page]
+                          (. js/document (getElementById "app"))))
 
 (defn logic-fn
   [msg data]
   (case msg
-    :uuid (println "My uuid is" (:uuid data))))
+    :uuid (do
+            (println "My uuid is" (:uuid data))
+            (render-page lobby))))
 
-(set! (.-onmessage socket) #(let [data (cljs.reader/read-string (.-data %))]
+(set! (.-onmessage socket) #(let [data (read-string (.-data %))]
                               (println data)
                               (logic-fn (:server-message data) data)))
 
@@ -18,7 +29,9 @@
   ([msg] (send-message msg {}))
   ([msg data] (.send socket (str (assoc data :client-message msg)))))
 
-(defn page []
+;;;;;;;;;;;;;;;;;;;; PAGES ;;;;;;;;;;;;;;;;;;;;
+(defn landing
+  []
   (let [state (atom {})]
     [:div
      [:label "Name"]
@@ -27,7 +40,15 @@
      [:label "Room Code"]
      [:input {:type "text"
               :on-change #(swap! state assoc :room-code (-> % .-target .-value))}]
-     [:button {:on-click #(send-message :join-room @state)} "Join Game"]]))
+     [:button {:on-click #(do (send-message :join-room @state)
+                              (swap! gamestate conj @state))} "Join Game"]]))
 
-(reagent/render-component [page]
-                          (. js/document (getElementById "app")))
+(def timer (atom 0))
+
+(js/setInterval #(swap! timer inc) 1000)
+
+(defn lobby
+  []
+  [:div "Welcome to the game " (:name @gamestate) "! The game will begin soon! The count is " @timer])
+
+(render-page landing)
