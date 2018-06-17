8574d4143fa14ab2423631953c74492445187095
Dane Johnson <dane_johnson@ymail.com>
2018-06-17 17:11:39 -0500

    Dynamic var now atom idk

--- src/clojure/parliment/core.clj
+++ src/clojure/parliment/core.clj
@@ -7,9 +7,10 @@
   (:gen-class))
 
 ;;;;;;;;;;;;;;;;;;;; ROOM ;;;;;;;;;;;;;;;;;;;;
-(defonce rooms (atom {}))
 
-(def ^:dynamic *reconnection-allowed* true)
+(defonce rooms (atom {}))
+(defonce channels (atom {}))
+(defonce reconnections-allowed (atom true))
 
 (defn generate-code
   []
@@ -31,7 +32,8 @@
 
 (defn join-room
   [room-code uuid channel name]
-  (swap! rooms update-in [room-code :players] assoc uuid {:channel channel :name name}))
+  (swap! rooms update-in [room-code :players] assoc uuid {:channel channel :name name})
+  (swap! channels assoc channel room-code))
 
 ;;;;;;;;;;;;;;;;;;;; WEBSOCKETS ;;;;;;;;;;;;;;;;;;;;
 
@@ -41,8 +43,10 @@
 
 (defn send-message-room
   ([room-code msg] (send-message-room room-code msg {}))
-  ([room-code msg data] (doseq [channel (map #(:channel %) (vals (get-in @rooms [room-code :players])))]
-                      (send-message channel msg data))))
+  ([room-code msg data]
+   (doseq [channel (map #(:channel %) (vals (get-in @rooms [room-code :players])))]
+     (send-message channel msg data))))
+
 (defn update-roster
   [room-code]
   (send-message-room room-code :update-roster
@@ -50,18 +54,20 @@
                                    (->> (map (fn [[uuid {name :name}]]
                                                {:uuid uuid :name name}))))}))
 
-(defn update-gamestate
+(defn update-mode
   [room-code]
-  (send-message-room))
+  (send-message-room room-code :update-mode {:mode (get-in @rooms [room-code :mode])}))
 
 (defn ws-handler
   [request]
   (with-channel request channel
     (on-close channel (fn [status]
-                        (println "channel closed: " status)))
+                        (println "channel closed: " status)
+                        (swap! channels dissoc channel)))
     (on-receive channel (fn [raw]
                           (let [data (clojure.edn/read-string raw)
-                                msg (:client-message data)]
+                                msg (:client-message data)
+                                room-code (get @channels channel)]
                             (case msg
                               :make-room (let [room-code (make-room)]
                                             (send-message channel :room-created {:room-code room-code}))
@@ -70,8 +76,9 @@
                                            (join-room (:room-code data) uuid channel (:name data))
                                            (update-roster (:room-code data)))
                               :reconnect (do
+                                           
                                            (if (and
-                                                *reconnection-allowed*
+                                                @reconnections-allowed
                                                 (contains? @rooms (:room-code data))
                                                 (contains? (get-in @rooms [(:room-code data) :players]) (:uuid data)))
                                              (do
@@ -87,7 +94,9 @@
   (files "" {:root "public"})
   (not-found "<p>404 PAGE NOT FOUND</p>"))
 
+(defonce shutdown (atom nil))
+
 (defn -main [& args]
-  (binding [*reconnection-allowed* false]
-    (run-server (site #'all-routes) {:port 8080})
-    (println "Server running!")))
+  (reset! reconnections-allowed false)
+  (reset! shutdown (run-server (site #'all-routes) {:port 8080}))
+  (println "Server running!"))
